49825edce80ac4d1fe98268b10be21486d06699c
Amrutha Ponduri <amruthasanthoshi2003@gmail.com>
2025-05-10 12:21:08 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_expend/firebase_options.dart';
+// import 'package:smart_expend/firebase_options.dart';
 void main() async{
   // WidgetsFlutterBinding.ensureInitialized();
   // if(kReleaseMode){
