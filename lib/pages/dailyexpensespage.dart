0f28508bb45d652e4feadfcc5b0c4b2b6e865c45
Amrutha Ponduri <amruthasanthoshi2003@gmail.com>
2025-05-11 15:55:14 +0530

    adding expenses successful

--- lib/pages/dailyexpensespage.dart
+++ lib/pages/dailyexpensespage.dart
@@ -1,80 +1,80 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-class DailyExpenses extends StatelessWidget {
+import 'package:clay_containers/clay_containers.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:smart_expend/widgets/addexpense_modal.dart';
+class DailyExpenses extends StatefulWidget {
   const DailyExpenses({super.key});
+
+  @override
+  State<DailyExpenses> createState() => _DailyExpensesState();
+}
+
+class _DailyExpensesState extends State<DailyExpenses> {
+  var expenseValue=<double>[
+      
+    ];
+    var expenseName = <String>[
+      
+    ];
   @override
   Widget build(BuildContext context) {
-    var price=[
-      100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,
-    ];
-    var expenseName = [
-      'Snacks',
-      'Food',
-      'Party',
-      'Beach',
-      'Momos',
-      'Snacks',
-      'Food',
-      'Party',
-      'Beach',
-      'Momos',
-      'Snacks',
-      'Food',
-      'Party',
-      'Beach',
-      'Momos',
-      'Snacks',
-      'Food',
-      'Party',
-      'Beach',
-      'Momos'
-    ];
+    
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 180, 200, 234),
+        backgroundColor: const Color.fromARGB(255, 180, 200, 234),
         title: const Text("Daily Expenses"),
         centerTitle: true,
       ),
-      body: ListView.separated(
+      body: expenseName.isEmpty?const Center(
+              child: Text('No espenses to display'),
+            ): ListView.separated(
         separatorBuilder: (context,index){
-          return SizedBox(height: 5,width: double.infinity,);
+          return const SizedBox(height: 0,width: double.infinity,);
           },
         itemCount: expenseName.length,
         scrollDirection: Axis.vertical,
         itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 200, 225, 245),
-              borderRadius: BorderRadius.circular(3),
-              // boxShadow: [
-              //   BoxShadow(
-              //     color: const Color.fromARGB(255, 172, 183, 194),
-              //     spreadRadius: 2,
-              //     blurRadius: 2,
-              //   )
-              // ]
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(child: Text(expenseName[index]),flex: 3,),
-                  Expanded(child: Text(price[index].toString()),flex: 3,),
-                  Expanded(flex: 1,child: Icon(
-                    Icons.delete,color: const Color.fromARGB(255, 45, 148, 232),
-                  ))
+          
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClayContainer(
               
-                ],
+              emboss: true,
+              borderRadius: 6,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(flex: 3,child: ClayText(expenseName[index],emboss: true,color: const Color.fromARGB(255, 145, 185, 218),),),
+                    Expanded(flex: 3,child: ClayText(expenseValue[index].toString(),emboss: true,color: const Color.fromARGB(255, 145, 185, 218),),),
+                    Expanded(flex: 1,child: InkWell(
+                      child: const Icon(
+                        Icons.delete,color:  Color.fromARGB(255, 145, 185, 218),
+                      ),onTap: () {}
+                    ))
+                
+                  ],
+                ),
               ),
             ),
           );
         }
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async{
+          await showMaterialModalBottomSheet(
+            context: context,
+            builder: (context) => AddexpenseModal(expenseName,expenseValue)
+          );
+          print(expenseName);
+          print(expenseValue);
+          setState(() {
+            
+          });
+        },
         backgroundColor: const Color.fromARGB(255, 180, 200, 234),
         child: const Icon(
           Icons.add,
