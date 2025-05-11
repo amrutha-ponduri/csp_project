cbe90ccb784af07a95e1d98b10baf8cb69ddfdb4
Amrutha Ponduri <amruthasanthoshi2003@gmail.com>
2025-05-11 07:22:54 +0530

    listview successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_expend/pages/dailyexpensespage.dart';
 // import 'package:smart_expend/firebase_options.dart';
 void main() async{
   // WidgetsFlutterBinding.ensureInitialized();
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DailyExpenses(),
     );
   }
 }
