939ae9d18f98398ecec6935dcbf6c476203a5331
G.jyothi <jyothigangapatla9@gmail.com>
2025-05-11 19:38:24 +0530

    Merge branch 'master' of https://github.com/amrutha-ponduri/csp_project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:smart_expend/pages/signuppage.dart';
 // import 'package\:firebase\_core/firebase\_core.dart';
+
 // import 'package\:flutter/foundation.dart';
 import 'package\:flutter/material.dart';
 
 // import 'package\:smart\_expend/firebase\_options.dart';
 void main() async {
-// WidgetsFlutterBinding.ensureInitialized();
+// WidgetsFlutterBinding.ensureInitialized();git
 // if(kReleaseMode){
 //   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
 // }
