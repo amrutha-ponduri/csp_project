0c3acf9584f5b25ebc94f9142db5d8f27f89978d
Amrutha Ponduri <amruthasanthoshi2003@gmail.com>
2025-05-11 15:52:16 +0530

    added widgets folder for saving modal bottom sheet

---
+++ lib/widgets/addexpense_modal.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AddexpenseModal extends StatefulWidget {
+  
+  final List<String> expenseNames;
+  final List<double> expenseValues;
+  const AddexpenseModal(this.expenseNames,this.expenseValues,{super.key});
+  @override
+  State<AddexpenseModal> createState() => _AddexpenseModalState();
+}
+
+class _AddexpenseModalState extends State<AddexpenseModal> {
+  final _formKey = GlobalKey<FormState>();
+  String expenseName = "";
+  double expenseValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 300,
+        child: Form(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextFormField(
+                autocorrect: true,
+                decoration: InputDecoration(
+                    hintText: 'What did you spend for?',
+                    hintFadeDuration: const Duration(milliseconds: 30),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                            const BorderSide(color: Colors.red, width: 1.5)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                          width: 1,
+                        ))),
+                validator: (value) {
+                  return validateExpenseName(value);
+                },
+                onSaved: (newValue) => expenseName=newValue!,
+              ),
+              TextFormField(
+                autocorrect: true,
+                decoration: InputDecoration(
+                    hintText: 'How much did you spend?',
+                    hintFadeDuration: const Duration(milliseconds: 30),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                            const BorderSide(color: Colors.red, width: 1.5)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                          width: 1,
+                        ))),       
+                validator: (value) {
+                  return validateExpenseValue(value);
+                },
+                onSaved: (newValue)=> expenseValue=double.parse(newValue!),
+              ),
+              ElevatedButton(onPressed: (){
+                if(_formKey.currentState!.validate()){
+                  _formKey.currentState!.save();
+                  print(expenseName);
+                  print(expenseValue);
+                  widget.expenseNames.add(expenseName);
+                  widget.expenseValues.add(expenseValue);
+                  Navigator.pop(context);
+                }
+              }, child: Text('Add'),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  validateExpenseName(String? value) {
+    if (value==null||value.isEmpty) {
+      return "Must fill an expense name";
+    }
+    return null;
+  }
+  
+  validateExpenseValue(String? value) {
+    if(value==null||value.isEmpty){
+      return "Must fill expense value";
+    }
+    if(double.tryParse(value)==null){
+      return "Enter valid expense value";
+    }
+    return null;
+  }
+}
